254a8ef8d5ebbc14062a91ea8dfbb136c493a0c9
Aki <reqlayy@gmail.com>
2024-09-05 11:51:15 +0300

    Add files via upload

---
+++ StygiaMedia.lua
@@ -0,0 +1,64 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+	 
+--statusbar
+LSM:Register("statusbar","Atrocity", [[Interface\Addons\StygiaMedia\statusbar\Atrocity]])
+LSM:Register("statusbar","Kait", [[Interface\Addons\StygiaMedia\statusbar\Kait.tga]])
+--sound
+LSM:Register("sound","|c00FF9696Blade|r", [[Interface\Addons\StygiaMedia\sound\Blade.ogg]])
+LSM:Register("sound","|c00FF9696Buffed|r", [[Interface\Addons\StygiaMedia\sound\Buffed.ogg]])
+LSM:Register("sound","|c00FF9696Bullet|r", [[Interface\Addons\StygiaMedia\sound\Bullet.ogg]])
+LSM:Register("sound","|c00FF9696Info|r", [[Interface\Addons\StygiaMedia\sound\Info.ogg]])
+LSM:Register("sound","|c00FF9696Mail|r", [[Interface\Addons\StygiaMedia\sound\Mail.ogg]])
+LSM:Register("sound","|c00FF9696Positive|r", [[Interface\Addons\StygiaMedia\sound\Positive.ogg]])
+LSM:Register("sound","|c00FF9696Thunder|r", [[Interface\Addons\StygiaMedia\sound\Thunder.ogg]])
+LSM:Register("sound","|c00FF9696On You|r", [[Interface\Addons\StygiaMedia\sound\OnYou.ogg]])
+LSM:Register("sound","|c00FF9696Sheesh|r", [[Interface\Addons\StygiaMedia\sound\sheesh.ogg]])
+LSM:Register("sound","|c00FF9696E|r", [[Interface\Addons\StygiaMedia\sound\e.ogg]])
+LSM:Register("sound","|c00FF9696Bruh|r", [[Interface\Addons\StygiaMedia\sound\bruh.ogg]])
+LSM:Register("sound","|c00FF9696Bababooey|r", [[Interface\Addons\StygiaMedia\sound\bababooey.ogg]])
+LSM:Register("sound","|c00FF9696Dreamscape|r", [[Interface\Addons\StygiaMedia\sound\dreamscape.mp3]])
+LSM:Register("sound","|c00FF9696Tokyo Drift|r", [[Interface\Addons\StygiaMedia\sound\tokyo_drift.ogg]])
+LSM:Register("sound","|c00FF9696Tacobell|r", [[Interface\Addons\StygiaMedia\sound\tacobell.mp3]])
+LSM:Register("sound","|c00FF9696Owen Wilson|r", [[Interface\Addons\StygiaMedia\sound\owenWilson.mp3]])
+LSM:Register("sound","|c00FF9696Smile1|r", [[Interface\Addons\StygiaMedia\sound\smile_WA.ogg]])
+LSM:Register("sound","|c00FF9696Smile2|r", [[Interface\Addons\StygiaMedia\sound\smile_WA2.ogg]])
+LSM:Register("sound","|c00FF9696Smile3|r", [[Interface\Addons\StygiaMedia\sound\smile_WA3.ogg]])
+LSM:Register("sound","|c00FF9696Skill Issue -Biden|r", [[Interface\Addons\StygiaMedia\sound\biden-skill-issue.mp3]])
+LSM:Register("sound","|c00FF9696Skill Issue1 -Ion|r", [[Interface\Addons\StygiaMedia\sound\frankly-BDGG.ogg]])
+LSM:Register("sound","|c00FF9696Half-Life Scream|r", [[Interface\Addons\StygiaMedia\sound\hl-scream.mp3]])
+LSM:Register("sound","|c00FF9696Lehmä 1|r", [[Interface\Addons\StygiaMedia\sound\lehmä1.mp3]])
+LSM:Register("sound","|c00FF9696Lehmä 2|r", [[Interface\Addons\StygiaMedia\sound\lehmä2.mp3]])
+LSM:Register("sound","|c00FF9696Aggro Human|r", [[Interface\Addons\StygiaMedia\sound\aggro.mp3]])
+LSM:Register("sound","|c00FF9696Neckers|r", [[Interface\Addons\StygiaMedia\sound\neckers.mp3]])
+LSM:Register("sound","|c00FF9696Tonttu Died|r", [[Interface\Addons\StygiaMedia\sound\tonttu-ied.mp3]])
+LSM:Register("sound","|c00FF9696Tonttu Balls|r", [[Interface\Addons\StygiaMedia\sound\tonttu-balls.mp3]])
+LSM:Register("sound","|c00FF9696Herra Armahda (Short)|r", [[Interface\Addons\StygiaMedia\sound\herra-armahda-short.mp3]])
+LSM:Register("sound","|c00FF9696Herra Armahda (Long)|r", [[Interface\Addons\StygiaMedia\sound\herra-armahda-long.mp3]])
+LSM:Register("sound","|c00FF9696Gock Sharing|r", [[Interface\Addons\StygiaMedia\sound\gock-sharing.mp3]])
+--WA Countdown
+LSM:Register("sound","|c00FF96961|r", [[Interface\AddOns\StygiaMedia\sound\countdown\1.ogg]])
+LSM:Register("sound","|c00FF96962|r", [[Interface\AddOns\StygiaMedia\sound\countdown\2.ogg]])
+LSM:Register("sound","|c00FF96963|r", [[Interface\AddOns\StygiaMedia\sound\countdown\3.ogg]])
+LSM:Register("sound","|c00FF96964|r", [[Interface\AddOns\StygiaMedia\sound\countdown\4.ogg]])
+LSM:Register("sound","|c00FF96965|r", [[Interface\AddOns\StygiaMedia\sound\countdown\5.ogg]])
+LSM:Register("sound","|c00FF96966|r", [[Interface\AddOns\StygiaMedia\sound\countdown\6.ogg]])
+LSM:Register("sound","|c00FF96967|r", [[Interface\AddOns\StygiaMedia\sound\countdown\7.ogg]])
+LSM:Register("sound","|c00FF96968|r", [[Interface\AddOns\StygiaMedia\sound\countdown\8.ogg]])
+LSM:Register("sound","|c00FF96969|r", [[Interface\AddOns\StygiaMedia\sound\countdown\9.ogg]])
+LSM:Register("sound","|c00FF969610|r", [[Interface\AddOns\StygiaMedia\sound\countdown\10.ogg]])
+--fonts
+LSM:Register("font","Expressway", [[Interface\Addons\StygiaMedia\fonts\Expressway.TTF]])
+LSM:Register("font","Naowh", [[Interface\Addons\StygiaMedia\fonts\Naowh.TTF]])
+--BigWigs Sounds
+BigWigsAPI:RegisterCountdown("StygiaMedia Countdown", {
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\1.ogg",
+    "Interface\\AddOns\\StygiaMedia\\sound\\countdown\\2.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\3.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\4.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\5.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\6.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\7.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\8.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\9.ogg",
+	"Interface\\AddOns\\StygiaMedia\\sound\\countdown\\10.ogg",
+})
